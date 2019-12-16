4442ec2dc607804437d8adbff594030ebf0e3754
Jonas <jonas.fo@live.se>
2019-12-16 13:11:34 +0100

    Added some documentation.

--- PokerProject/PokerClient/Client.py
+++ PokerProject/PokerClient/Client.py
@@ -2,7 +2,7 @@ __author__ = 'Jonas Fockstedt, Ema Krcic'
 
 import socket
 import random
-from collections import Counter 
+from collections import Counter
 
 import ClientBase
 
@@ -15,8 +15,10 @@ BUFFER_SIZE = 1024
 POKER_CLIENT_NAME = 'Agent16'
 CURRENT_HAND = []
 
-cardRanks = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
+cardRanks = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
+             "8": 8, "9": 9, "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
 possibleHands = {}
+
 
 class pokerGames(object):
     def __init__(self):
@@ -28,13 +30,11 @@ class pokerGames(object):
         self.handStrength = 0
         self.handRank = ''
 
-
     def returnSuit(self, card):
         '''
         Specifically called when the hand needs to be sorted after suits.
         '''
         return card[1]
-
 
     def sortHand(self):
         '''
@@ -42,7 +42,6 @@ class pokerGames(object):
         '''
         self.CurrentHand.sort()
         self.CurrentHand.sort(key=self.returnSuit)
-    
 
     def calculateHand(self):
         strength = 0
@@ -56,14 +55,12 @@ class pokerGames(object):
 
         print('Hand rank of agent:')
         print(self.handRank)
-        
 
         for card in self.CurrentHand:
-            strength+=cardRanks[card[0]]
+            strength += cardRanks[card[0]]
         self.handStrength = strength
 
         print('Strength of hand is: ' + str(self.handStrength))
-
 
     def checkStraight(self):
         '''
@@ -72,23 +69,24 @@ class pokerGames(object):
         self.checkFlush()
         # Check if hand is a flush, then determine if it is a straight flush, or regular flush.
         if cardRanks[self.CurrentHand[0][0]]+1 == cardRanks[self.CurrentHand[1][0]] and cardRanks[self.CurrentHand[1][0]]+1 == cardRanks[self.CurrentHand[2][0]] \
-            and cardRanks[self.CurrentHand[2][0]]+1 == cardRanks[self.CurrentHand[3][0]] and cardRanks[self.CurrentHand[3][0]]+1 == cardRanks[self.CurrentHand[4][0]]:
+                and cardRanks[self.CurrentHand[2][0]]+1 == cardRanks[self.CurrentHand[3][0]] and cardRanks[self.CurrentHand[3][0]]+1 == cardRanks[self.CurrentHand[4][0]]:
             if self.handRank == 'Flush':
-                print('********STRAIGHT FLUSH!!!!********') # Assign value to agents hand rank.
+                # Assign value to agents hand rank.
+                print('********STRAIGHT FLUSH!!!!********')
                 self.handRank = 'Straight flush'
             else:
                 print('********STRAIGHT!!!!********')
-                self.handRank = 'Straight'                  # Assign value to agents hand rank.
-        
-    
+                # Assign value to agents hand rank.
+                self.handRank = 'Straight'
+
     def checkFlush(self):
         '''
         Check whether the agent has a flush.
         '''
         if self.CurrentHand[0][1] == self.CurrentHand[1][1] == self.CurrentHand[2][1] == self.CurrentHand[3][1] == self.CurrentHand[4][1]:
             print('********FLUSH!!!!********')
-            self.handRank = 'Flush'                         # Assign value to agents hand rank.
-
+            # Assign value to agents hand rank.
+            self.handRank = 'Flush'
 
     def checkPairs(self):
         '''
@@ -96,55 +94,62 @@ class pokerGames(object):
         '''
         rankOccurences = {}
         numberOfPairs = 1
-        twoPairs = False                                        # To check if agent has two pairs.
-        threePairs = False                                      # To check if agent has three pairs.
-        self.handRank = ''                                      # Reset hand rank.
+        # To check if agent has two pairs.
+        twoPairs = False
+        # To check if agent has three pairs.
+        threePairs = False
+        # Reset hand rank.
+        self.handRank = ''
 
         # card[0] corresponds to rank of first card.
         for card in self.CurrentHand:
             if card[0] in rankOccurences:
-                rankOccurences[card[0]]+=1                      # Increase number of occurences.
+                # Increase number of occurences.
+                rankOccurences[card[0]] += 1
             else:
-                rankOccurences[card[0]] = 1                     # If first occurence.
+                # If first occurence.
+                rankOccurences[card[0]] = 1
 
-
-        listOfDict = list(rankOccurences.items())               # Convert the dictionary to a list.
+        # Convert the dictionary to a list.
+        listOfDict = list(rankOccurences.items())
         for occurence in listOfDict:
             if occurence[1] > numberOfPairs:
                 numberOfPairs = occurence[1]
 
-
         # Revisit this at some point.
             for rank in listOfDict:
                 # Index 1 corresponds to the number of occurences of a given card.
-                if rank[1] == 2:                                # If a rank has occured two times.
+                # If a rank has occured two times.
+                if rank[1] == 2:
                     twoPairs = True
-                elif rank[1] == 3:                              # If a rank has occured three times.
+                # If a rank has occured three times.
+                elif rank[1] == 3:
                     threePairs = True
-            
-            
+
         if threePairs and twoPairs:
             print('********FULL HOUSE!!!!********')
-            self.handRank == 'Full house.'                      # Assign value to agents hand rank.
+            # Assign value to agents hand rank.
+            self.handRank == 'Full house.'
         elif twoPairs:
             print('Two pairs!!!')
-            self.handRank = 'Two pairs'                         # Assign value to agents hand rank.
+            # Assign value to agents hand rank.
+            self.handRank = 'Two pairs'
         # Check if one pair.
         if numberOfPairs == 2:
-            self.handRank = 'One Pair'                          # Assign value to agents hand rank.
+            # Assign value to agents hand rank.
+            self.handRank = 'One Pair'
         # Check if two pairs.
         elif numberOfPairs == 3:
-            self.handRank = 'Three of a kind'                   # Assign value to agents hand rank.
+            # Assign value to agents hand rank.
+            self.handRank = 'Three of a kind'
         elif numberOfPairs == 4:
-            self.handRank = 'Four of a kind'                    # Assign value to agents hand rank.
-        
-            
+            # Assign value to agents hand rank.
+            self.handRank = 'Four of a kind'
 
         print('Number of occurences:')
         print(rankOccurences)
-        
 
-    def queryPlayerName(self,_name):
+    def queryPlayerName(self, _name):
         '''
         Gets the name of the player.\n
         return - The name of the player as a single word without space. <code>null</code> is not a valid answer.
@@ -153,7 +158,6 @@ class pokerGames(object):
             _name = POKER_CLIENT_NAME
         return _name
 
-    
     def queryOpenAction(self, _minimumPotAfterOpen, _playersCurrentBet, _playersRemainingChips):
         '''
         Modify queryOpenAction() and add your strategy here\n
@@ -177,12 +181,12 @@ class pokerGames(object):
         # If hand is too weak, check.
         if self.handStrength <= 10:
             return ClientBase.BettingAnswer.ACTION_CHECK
-    
+
         # Random Open Action
         def chooseOpenOrCheck():
             if _playersCurrentBet + _playersRemainingChips > _minimumPotAfterOpen:
-                #return ClientBase.BettingAnswer.ACTION_OPEN,  iOpenBet
-                return ClientBase.BettingAnswer.ACTION_OPEN,  (random.randint(0, 10) + _minimumPotAfterOpen) if _playersCurrentBet + _playersRemainingChips + 10> _minimumPotAfterOpen else _minimumPotAfterOpen
+                # return ClientBase.BettingAnswer.ACTION_OPEN,  iOpenBet
+                return ClientBase.BettingAnswer.ACTION_OPEN,  (random.randint(0, 10) + _minimumPotAfterOpen) if _playersCurrentBet + _playersRemainingChips + 10 > _minimumPotAfterOpen else _minimumPotAfterOpen
             else:
                 return ClientBase.BettingAnswer.ACTION_CHECK
 
@@ -191,7 +195,6 @@ class pokerGames(object):
             1: ClientBase.BettingAnswer.ACTION_ALLIN,
         }.get(random.randint(0, 2), chooseOpenOrCheck())
 
-    
     def queryCallRaiseAction(self, _maximumBet, _minimumAmountToRaiseTo, _playersCurrentBet, _playersRemainingChips):
         '''
         Modify queryCallRaiseAction() and add your strategy here.\n
@@ -215,9 +218,11 @@ class pokerGames(object):
         print("Player requested to choose a call/raise action.")
         self.calculateHand()
         # Random Open Action
+
         def chooseRaiseOrFold(self):
-            if  _playersCurrentBet + _playersRemainingChips > _minimumAmountToRaiseTo:
-                return ClientBase.BettingAnswer.ACTION_RAISE,  (random.randint(0, 10) + _minimumAmountToRaiseTo) if _playersCurrentBet+ _playersRemainingChips + 10 > _minimumAmountToRaiseTo else _minimumAmountToRaiseTo
+            # Check if agent can afford to join the next round. Else, fold.
+            if _playersCurrentBet + _playersRemainingChips > _minimumAmountToRaiseTo:
+                return ClientBase.BettingAnswer.ACTION_RAISE,  (random.randint(0, 10) + _minimumAmountToRaiseTo) if _playersCurrentBet + _playersRemainingChips + 10 > _minimumAmountToRaiseTo else _minimumAmountToRaiseTo
             else:
                 return ClientBase.BettingAnswer.ACTION_FOLD
         return {
@@ -226,39 +231,35 @@ class pokerGames(object):
             2: ClientBase.BettingAnswer.ACTION_CALL if _playersCurrentBet + _playersRemainingChips > _maximumBet else ClientBase.BettingAnswer.ACTION_FOLD
         }.get(random.randint(0, 3), chooseRaiseOrFold(self))
 
-    
     def queryCardsToThrow(self, _hand):
         '''
-        Modify queryCardsToThrow() and add your strategy to throw cards
+        Modify queryCardsToThrow() and add your strategy to throw cards\n
         Called during the draw phase of the game when the player is offered to throw away some
-        (possibly all) of the cards on hand in exchange for new.
+        (possibly all) of the cards on hand in exchange for new.\n
         return - An array of the cards on hand that should be thrown away in exchange for new, or 
         <code>null</code> or an empty array to keep all cards.\n
         @see     #infoCardsInHand(ca.ualberta.cs.poker.Hand)
         '''
         print("Requested information about what cards to throw")
         print(_hand)
-        return _hand[random.randint(0,4)] + ' '
+        return _hand[random.randint(0, 4)] + ' '
 
     # InfoFunction:
 
-    
     def infoNewRound(self, _round):
         '''
         Called when a new round begins.\n
         round - the round number (increased for each new round).
         '''
         #_nrTimeRaised = 0
-        print('Starting Round: ' + _round )
+        print('Starting Round: ' + _round)
 
-        
     def infoGameOver(self):
         '''
         Called when the poker server informs that the game is completed.
         '''
         print('The game is over.')
 
-    
     def infoPlayerChips(self, _playerName, _chips):
         '''
         Called when the server informs the players how many chips a player has.\n
@@ -267,7 +268,6 @@ class pokerGames(object):
         '''
         print('The player ' + _playerName + ' has ' + _chips + 'chips')
 
-        
     def infoAnteChanged(self, _ante):
         '''
         Called when the ante has changed.\n
@@ -275,77 +275,71 @@ class pokerGames(object):
         '''
         print('The ante is: ' + _ante)
 
-    
     def infoForcedBet(self, _playerName, _forcedBet):
         '''
         Called when a player had to do a forced bet (putting the ante in the pot).\n
         playerName - the name of the player forced to do the bet.\n
         forcedBet - the number of chips forced to bet.
         '''
-        print("Player "+ _playerName +" made a forced bet of "+ _forcedBet + " chips.")
+        print("Player " + _playerName +
+              " made a forced bet of " + _forcedBet + " chips.")
 
-
-    
     def infoPlayerOpen(self, _playerName, _openBet):
         '''
         Called when a player opens a betting round.\n
         playerName - the name of the player that opens.\n
         openBet - the amount of chips the player has put into the pot.
         '''
-        print("Player "+ _playerName + " opened, has put "+ _openBet +" chips into the pot.")
+        print("Player " + _playerName + " opened, has put " +
+              _openBet + " chips into the pot.")
 
-    
     def infoPlayerCheck(self, _playerName):
         '''
         Called when a player checks.\n
         playerName - the name of the player that checks.
         '''
-        print("Player "+ _playerName +" checked.")
+        print("Player " + _playerName + " checked.")
 
-    
     def infoPlayerRise(self, _playerName, _amountRaisedTo):
         '''
         Called when a player raises.\n
         playerName - the name of the player that raises.\n
         amountRaisedTo - the amount of chips the player raised to.
         '''
-        print("Player "+_playerName +" raised to "+ _amountRaisedTo+ " chips.")
+        print("Player "+_playerName + " raised to " +
+              _amountRaisedTo + " chips.")
 
-    
     def infoPlayerCall(self, _playerName):
         '''
         Called when a player calls.\n
         playerName - the name of the player that calls.
         '''
-        print("Player "+_playerName +" called.")
+        print("Player "+_playerName + " called.")
 
-    
     def infoPlayerFold(self, _playerName):
         '''
         Called when a player folds.\n
         playerName - the name of the player that folds.
         '''
-        print("Player "+ _playerName +" folded.")
+        print("Player " + _playerName + " folded.")
 
-    
     def infoPlayerAllIn(self, _playerName, _allInChipCount):
         '''
         Called when a player goes all-in.\n
         playerName - the name of the player that goes all-in.\n
         allInChipCount - the amount of chips the player has in the pot and goes all-in with.
         '''
-        print("Player "+_playerName +" goes all-in with a pot of "+_allInChipCount+" chips.")
+        print("Player "+_playerName + " goes all-in with a pot of " +
+              _allInChipCount+" chips.")
 
-    
     def infoPlayerDraw(self, _playerName, _cardCount):
         '''
         Called when a player has exchanged (thrown away and drawn new) cards.\n
         playerName - the name of the player that has exchanged cards.\n
         cardCount - the number of cards exchanged.
         '''
-        print("Player "+ _playerName + " exchanged "+ _cardCount +" cards.")
+        print("Player " + _playerName + " exchanged " + _cardCount + " cards.")
 
-    
     def infoPlayerHand(self, _playerName, _hand):
         '''
         Called during the showdown when a player shows his hand.\n
@@ -353,20 +347,19 @@ class pokerGames(object):
         hand - the players hand.
         '''
         self.sortHand()
-        #self.CurrentHand.sort()
-        #self.CurrentHand.sort(key=self.returnSuit)
-        print("Player "+ _playerName +" hand " + str(_hand))
+        # self.CurrentHand.sort()
+        # self.CurrentHand.sort(key=self.returnSuit)
+        print("Player " + _playerName + " hand " + str(_hand))
 
-    
     def infoRoundUndisputedWin(self, _playerName, _winAmount):
         '''
         Called during the showdown when a players undisputed win is reported.\n
         playerName - the name of the player whose undisputed win is anounced.\n
         winAmount - the amount of chips the player won.
         '''
-        print("Player "+ _playerName +" won "+ _winAmount +" chips undisputed.")
+        print("Player " + _playerName + " won " +
+              _winAmount + " chips undisputed.")
 
-    
     def infoRoundResult(self, _playerName, _winAmount):
         '''
         Called during the showdown when a players win is reported. If a player does not win anything,
@@ -374,5 +367,4 @@ class pokerGames(object):
         playerName - the name of the player whose win is anounced.\n
         winAmount - the amount of chips the player won.
         '''
-        print("Player "+ _playerName +" won " + _winAmount + " chips.")
-
+        print("Player " + _playerName + " won " + _winAmount + " chips.")
--- PokerProject/PokerClient/agent.md
+++ PokerProject/PokerClient/agent.md
@@ -1,40 +1,55 @@
-# IDEAS
+# FEATURES
+
+- Agent can determine which hand it has.
 
 ## TODO
+
 - [ ] Rank hand.
-    - [x] Determine the possible hands.
-        - [x] High cards.
-        - [x] One pair.
-        - [x] Two pairs.
-        - [x] Three of a kind.
-        - [x] Straight.
-        - [x] Flush.
-        - [x] Full house.
-        - [x] Four of a kind.
-        - [x] Straight flush.
-    - [x] Rank actual strength (adding value of ranks).
-    - [ ] Rank potential strength.
+  - [x] Determine the possible hands.
+    - [x] High cards.
+    - [x] One pair.
+    - [x] Two pairs.
+    - [x] Three of a kind.
+    - [x] Straight.
+    - [x] Flush.
+    - [x] Full house.
+    - [x] Four of a kind.
+    - [x] Straight flush.
+  - [ ] Rank strength of hand.
+    - [ ] Give rank of the different type of hands.
+    - [ ] Calculate strength of hand when agent has 'high cards'.
+  - [ ] Rank potential strength.
 - [ ] When to "check" during opening round (if first).
 - [ ] Handle cases when Aces are low (when part of straight or straight flush).
 
+## IDEAS
+
 ### Hand
+
 1. "Actual" ranking system. Used for determining the strength of current hand.
 2. "Potential" ranking system. Used for determining how good the hand can become.
 
 ### Open Action
+
 1. Depending of strength of hand.
-2. *Check* if low on chips (and first), to prevent unnecessary betting.
+2. _Check_ if low on chips (and first), to prevent unnecessary betting.
 
 ### Call / Raise Action
+
 1. Based on number of chips left for the agent.
 2. How close to a certain hand, i.e. if a possible good hand could be one round away, make sure to stay in the game.
 3. Check current pot.
 
 ### Cards To Throw
-1. Determine how close to a certain hand. 
+
+1. Determine how close to a certain hand, called "goal" hand.
 2. How far away the "goal" hand is (in number of rounds).
 3. Have a way to mark a card as important (cards not to throw away). Could be in a separate list.
 
 ### Fold
+
 1. If a certain percentage of chips has been bet during current round to a point that it is no longer sufficient.
 
+### Check
+
+1. When hand is mediocre.
